c5a0e043a2ff284483c1df78ebedad36cce2dd62
patt509 <luca.lucci@studenti.unina.it>
2025-11-21 14:32:14 +0100

    Add hash table functions in the header file

--- include/hashtable.h
+++ include/hashtable.h
@@ -17,12 +17,67 @@ typedef struct HashTable {
    int count;        // Total elements inserted
 } HashTable;
 
-// Function that creates the table and initialize every string to NULL
-HashTable* hash_table_create(int size);
+/*
+   Function that creates the table.
 
-// Function that inserts an element into the table
-// It creates the node with the key and the value and then calculates the
-// hash index to insert the node at
-bool hash_table_insert(HashTable* table, char* key, char* value);
+   It accepts the initial size of the table, allocates
+   the buckets. It sets every bucket to NULL because
+   non initialized pointers can point to random
+   data and this would mess up the program.
+*/
+HashTable* ht_create(int size);
+
+/*
+   Hash function that produces the corresponding index.
+
+   Accepts only the string value.
+   - the specific hash function needs to be defined -
+*/
+ulong hash(char* value);
+
+/*
+   Helper for the insert function.
+
+   Accepts input value and key.
+   Allocates a new node, allocates memory for the value
+   and for the key (deep copy) and saves them in the node
+   fields.
+   Sets the next pointer of the node to NULL and return
+   the pointer to the just created node.
+*/
+Node* ht_create_pair(char* value, char* key);
+
+/*
+   Function that inserts a node into the table.
+
+   Accept a pointer to the table, the key and the value.
+   Calls the hash function passing the key to it to obtain
+   the corresponding index. Then it goes to the bucket at
+   the found index and iterates through the bucket until:
+   1) A node with the same key is found: in this case the
+      previuos node has to be freed and replaced with
+      the new node.
+   2) If a node with the same key is not found, the new
+      node will be insterted at the head of the bucket.
+   It calls the helper to create a new node. The "next"
+   pointer of the new node will point to the previous
+   first node and the bucket needs to point to the new node.
+   Then increments the counter of total elements.
+*/
+bool ht_insert(HashTable* table, char* key, char* value);
+
+/*
+   Function that returns a pointer to the value the
+   searched node is storing (node->value).
+   If not found, it will return NULL.
+
+   Accepts a pointer to the table and the key.
+   Calls the hash function to calculate the index of the
+   bucket to check and iterates though that bucket:
+   confronts every key with string comparing functions.
+   If a match is found, the function will return char* value.
+   If the whole bucket does not contain the key, return NULL.
+*/
+char* ht_get(HashTable* table, char* key);
 
 #endif
---
+++ src/hashtable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Including corresponding header file
+#include "../include/hashtable.h"
+
+HashTable* ht_create(int size) {
+   
+}
