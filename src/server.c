a12c7d568eb642cb4648585f311ba93acffb20d5
patt509 <luca.lucci@studenti.unina.it>
2025-11-24 09:29:47 +0100

    Implement trim_newline function and stub for parse_and_execute

--- src/server.c
+++ src/server.c
@@ -6,8 +6,22 @@
 #define MAX_CMD_SIZE 1024
 
 // First function
+void trim_newline (char* cmd) {
+   // Get the lenght of the command inserted
+   // Calling strlen only one time (time complexity O(N))
+   size_t len = strlen(cmd);
+
+   // Check if the last character is the new line character
+   if (len > 0 && cmd[len - 1] == '\n') {
+      // If it is, replace it with \0
+      cmd[len - 1] = '\0';
+   }
+}
 
 // Second function
+char** parse_and_execute (char* cmd) {
+
+}
 
 // Start server function
 void server_run (HashTable* table) {
